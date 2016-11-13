d29e38b583133e2a6896daa29129a98574a1cd76
vidartf <vidartf@gmail.com>
2016-11-13 13:34:25 +0100

    Ensure proper version formatting

--- constructor/winmsi.py
+++ constructor/winmsi.py
@@ -202,6 +202,7 @@ def make_wxs(info, dir_path):
     replace = {
         'NAME': name,
         'VERSION': info['version'],
+        'VIPV': make_VIProductVersion(info['version']),
         'COMPANY': info.get('company', 'Unknown'),
         'PRODUCT_GUID': product_uuid,
         'UPGRADE_GUID': upgrade_uuid,
