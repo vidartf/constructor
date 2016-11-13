d054c3570da9c42d8fbd75cf348a01d30341e3d8
vidartf <vidartf@gmail.com>
2016-11-13 14:51:04 +0100

    Lint

--- constructor/winmsi.py
+++ constructor/winmsi.py
@@ -6,14 +6,13 @@
 
 from __future__ import print_function, division, absolute_import
 
-import os
 import sys
 import shutil
 import tempfile
 import tarfile
 from itertools import chain
 from six.moves import zip_longest
-from os.path import abspath, dirname, isfile, isdir, join, split, splitext
+from os.path import abspath, dirname, isfile, join, splitext
 from subprocess import check_call, check_output
 import uuid
 from xml.sax.saxutils import escape as escape_xml
@@ -33,20 +32,24 @@ HEAT_EXE = join(sys.prefix, 'wix', 'heat.exe')
 
 EXTENSIONS = ['WixUIExtension', 'WixUtilExtension']
 
-# WARNING: DO NOT MODIFY THIS UUID, AS IT WILL BREAK 
+# WARNING: DO NOT MODIFY THIS UUID, AS IT WILL BREAK
 # BACKWARDS COMPATIBILITY OF ALL GENERATED INSTALLERS
 # IF YOU DO!
-CONSTRUCTOR_UUID_NAMESPACE = uuid.UUID('{00dde158-c9df-4fb2-b4d0-b363906936ac}')
+CONSTRUCTOR_UUID_NAMESPACE = uuid.UUID(
+    '{00dde158-c9df-4fb2-b4d0-b363906936ac}')
 
 
 def namespace_uuid(name):
     return str(uuid.uuid5(CONSTRUCTOR_UUID_NAMESPACE, name)).upper()
 
+
 def random_uuid():
     return str(uuid.uuid4())
 
+
 def escape_id(id):
     return id.replace('-', '')
+
 
 def read_wxs_tmpl():
     path = join(WIX_DIR, 'template.wxs')
@@ -74,7 +77,7 @@ def properties(info, dir_path):
                     ('UrlsTxtFile', 'urls.txt'),
                     ('PostInstall', 'post_install.bat')]:
         properties[key] = fn
-    
+
     for key, value in properties.items():
         value = escape_xml(value)
         yield "<?define %s='%s'?>" % (key, value)
@@ -116,7 +119,7 @@ def harvest_packages(path, dir_id, outdir=None):
         # Fragment file:
         '-o', dest_file,
         # Suppress COM elements, fragments, root directory as element,
-        # registry harvesting (these options will create a grouping 
+        # registry harvesting (these options will create a grouping
         # that most applications can use)
         '-scom', '-sfrag', '-srd', '-sreg', '-ke',
         # Prevent header spamming:
@@ -140,16 +143,13 @@ def pkg2component(download_dir, tmp_dir, dists, py_version):
     harvest_packages(join(tmp_dir, 'unpack'), 'pkgs', tmp_dir)
     for i, fn in enumerate(vs_dists + dists):
         name, version, unused_build = fn.rsplit('-', 2)
-        comp_id = escape_xml(escape_id(name))
         dir_id = escape_xml(escape_id(name + '_FOLDER'))
         extracted_folder = fn.replace('.tar.bz2', '')
         assert len(extracted_folder) > 0
-        if i == 0: # MSVC runtimes
+        if i == 0:  # MSVC runtimes
             assert 'runtime' in fn
-            comp_id = 'MSVC'
-        elif i == 1: # Python
+        elif i == 1:  # Python
             assert fn.startswith('python-')
-            comp_id = 'Python'
         elif fn == vs_dists[0]:
             continue
         yield "<Directory Id='%s' Name='%s' />" % (
@@ -160,26 +160,12 @@ def pkg2component_refs(dists, py_version):
     vs_dists = find_vs_runtimes(dists, py_version)
     for i, fn in enumerate(vs_dists + dists):
         name, version, unused_build = fn.rsplit('-', 2)
-        if i < 2: # MSVC runtimes or Python
+        if i < 2:  # MSVC runtimes or Python
             continue  # Included manually in template
         elif fn == vs_dists[0]:
             continue
         else:
             yield "<ComponentGroupRef Id='%s' />" % escape_xml(escape_id(name))
-
-
-def harvested_fragment_args(dists, py_version, tmp_dir, ext):
-    vs_dists = find_vs_runtimes(dists, py_version)
-    for i, fn in enumerate(vs_dists + dists):
-        if i == 0: # MSVC runtimes
-            yield join(tmp_dir, 'MSVC' + ext)
-        elif i == 1: # Python
-            yield join(tmp_dir, 'Python' + ext)
-        elif fn == vs_dists[0]:
-            continue
-        else:
-            name, unused_version, unused_build = fn.rsplit('-', 2)
-            yield join(tmp_dir, escape_xml(escape_id(name)) + ext)
 
 
 def make_wxs(info, dir_path):
@@ -188,15 +174,13 @@ def make_wxs(info, dir_path):
     major_version = info['version'].split('.', 1)[0]
     # All updates are "major" updates:
     product_uuid = str(uuid.uuid4())
-    # Note: Add a token to name from our side if we/conda ever 
+    # Note: Add a token to name from our side if we/conda ever
     # break major update capability:
     upgrade_uuid = namespace_uuid(name + major_version)
     download_dir = info['_download_dir']
     dists = info['_dists']
     py_name, py_version, unused_build = dists[0].rsplit('-', 2)
     assert py_name == 'python'
-    arch = int(info['_platform'].split('-')[1])
-    web_cmd = WEB_ENVIRONMENT_CMD if 'web_environment' in info else ''
 
     # these appear as __<key>__ in the template, and get escaped
     replace = {
@@ -224,9 +208,8 @@ def make_wxs(info, dir_path):
         ('@PROPERTIES@', '\n  '.join(props)),
         ('@PKG_COMPONENTS@', '\n          '.join(components)),
         ('@PKG_COMPONENTS_REFS@', '\n      '.join(comp_refs)),
-        ('@WEB_ENVIRONMENT@', '\n    '.join(web_cmd.splitlines())),
         ('@MENU_PKGS@', ' '.join(info.get('menu_packages', []))),
-        ]:
+    ]:
         data = data.replace(key, value)
 
     wxs_path = join(dir_path, 'main.wxs')
@@ -264,13 +247,11 @@ def create(info):
     verify_wix_install()
     # tmp_dir = tempfile.mkdtemp()
     tmp_dir = 'c:\\github\\ctorout\\build\\'
-    outfile = info['_outpath'];
-    license_file = abspath(info.get('license_file',
-        join(WIX_DIR, 'placeholder_license.txt'))),
-    welcome_image = info.get('welcome_image', None)
-    header_image = info.get('header_image', None)
+    outfile = info['_outpath']
+    license_file = abspath(info.get(
+        'license_file',
+        join(WIX_DIR, 'placeholder_license.txt')))
     preconda.write_files(info, tmp_dir)
-    download_dir = info['_download_dir']
     dists = info['_dists']
     py_version = dists[0].rsplit('-', 2)[1]
     unpack(download_dir, tmp_dir, dists)
@@ -291,9 +272,6 @@ def create(info):
     write_images(info, tmp_dir)
 
     wxs = make_wxs(info, tmp_dir)
-
-    fragments = list(harvested_fragment_args(
-        dists, py_version, tmp_dir, '.wxs'))
     harvet_wxs = join(tmp_dir, 'harvest.wxs')
     args = [CANDLE_EXE, '-out', tmp_dir, harvet_wxs, wxs]
     print('Calling: %s' % args)
@@ -316,7 +294,7 @@ def create(info):
 
 
 if __name__ == '__main__':
-    make_nsi({'name': 'Maxi', 'version': '1.2',
+    make_wxs({'name': 'Maxi', 'version': '1.2',
               '_platform': 'win-64',
               '_outpath': 'dummy.exe',
               '_download_dir': 'dummy',
